54903e06e156853283af9b6ef7fca6eb0ae8edb0
Greg-Beaucaire <greg.beaucaire@gmail.com>
2020-11-10 13:39:43 +0100

    Add files via upload
    Grand moment dans la vie de Github

--- adult-remote/page-telecommande.js
+++ adult-remote/page-telecommande.js
@@ -2,33 +2,33 @@
 
 // j'écoute l'évènement "click" de la première touche de ma télécommande
 document.querySelector("main > ul > li:nth-of-type(1)").addEventListener("click", (event) => {
-  let bruit = new Audio("sons/kid/cat.mp3"); // je créé un objet audio appelé "bruit"
+  let bruit = new Audio("sons/adult/cestclair.mp3"); // je créé un objet audio appelé "bruit"
   bruit.play(); // je joue ce bruit
 });
 
-// même chose avec les autres touches de ma télécommande
+// même chose avec les autres touches de ma télécommande²²
 
 document.querySelector("main > ul > li:nth-of-type(2)").addEventListener("click", (event) => {
-  let bruit = new Audio("sons/kid/chien.mp3");
+  let bruit = new Audio("sons/adult/chirac.mp3");
   bruit.play();
 });
 
 document.querySelector("main > ul > li:nth-of-type(3)").addEventListener("click", (event) => {
-  let bruit = new Audio("sons/kid/elephant.mp3");
+  let bruit = new Audio("sons/adult/honteux.mp3");
   bruit.play();
 });
 
 document.querySelector("main > ul > li:nth-of-type(4)").addEventListener("click", (event) => {
-  let bruit = new Audio("sons/kid/clochette.mp3");
+  let bruit = new Audio("sons/adult/non.mp3");
   bruit.play();
 });
 
 document.querySelector("main > ul > li:nth-of-type(5)").addEventListener("click", (event) => {
-  let bruit = new Audio("sons/kid/klaxon.mp3");
+  let bruit = new Audio("sons/adult/sucer.mp3");
   bruit.play();
 });
 
 document.querySelector("main > ul > li:nth-of-type(6)").addEventListener("click", (event) => {
-  let bruit = new Audio("sons/kid/santa.mp3");
+  let bruit = new Audio("sons/adult/taisezvous.mp3");
   bruit.play();
 });
